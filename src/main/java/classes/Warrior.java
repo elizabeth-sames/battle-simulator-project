f3cbc1c2f9871ef9276d2345bc4d1d8cc8d622af
LauraAdom <laura.adomaityt@gmail.com>
2022-04-23 10:02:04 +0200

    updated-setters

--- src/main/java/classes/Warrior.java
+++ src/main/java/classes/Warrior.java
@@ -4,12 +4,60 @@ public class Warrior extends Character {
     private int stamina;
     private int strength;
 
+
+
+    //CONSTRUCTOR
     public Warrior(int id, String name, int hp, int stamina, int strength) {
         super(id, name, hp);
         this.stamina = stamina;
         this.strength = strength;
+        setStamina(stamina);
+        setStrength(strength);
+        setHp(hp);
     }
 
+
+
+    //SETTERS
+    public void setStrength(int strength) {
+        if(strength >= 1 && strength <= 10){
+            this.strength = strength;
+        } else {
+            System.out.println("Warrior's strength needs to be in between 1 and 10 both inclusive, insert other number.");
+        }
+    }
+
+    public void setStamina(int stamina) {
+        if(stamina >= 10 && stamina <= 50){
+            this.stamina = stamina;
+        } else {
+            System.out.println("Warrior's stamina needs to be in between 10 and 50 both inclusive, insert other number.");
+        }
+    }
+
+    @Override
+    public void setHp(int hp){
+        if(hp >= 100 && hp <= 200){
+            super.setHp(hp);
+        } else {
+            System.out.println("Warrior's hp needs to be in between 100 and 200 both inclusive, insert other number.");
+        }
+    }
+
+
+
+    //GETTERS
+    public int getStamina() {
+        return stamina;
+    }
+    public int getStrength() {
+        return strength;
+    }
+
+
+
+
+    //METHODS
     public int attack() {
         int damage = this.stamina;
         if(this.stamina>5){
@@ -23,19 +71,4 @@ public class Warrior extends Character {
         }
     }
 
-    public int getStamina() {
-        return stamina;
-    }
-
-    public void setStamina(int stamina) {
-        this.stamina = stamina;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
 }
--- src/main/java/classes/Wizard.java
+++ src/main/java/classes/Wizard.java
@@ -1,15 +1,64 @@
 package classes;
 
+import java.util.random.RandomGenerator;
+
 public class Wizard extends Character{
     private int mana;
     private int intelligence;
 
+
+    
+    //CONSTRUCTOR
     public Wizard(int id, String name, int hp, int mana, int intelligence) {
         super(id, name, hp);
         this.mana = mana;
         this.intelligence = intelligence;
+        setIntelligence(intelligence);
+        setHp(hp);
+        setMana(mana);
     }
 
+
+
+    //SETTERS
+    public void setIntelligence(int intelligence) {
+        if(intelligence >= 1 && intelligence <= 50){
+            this.intelligence = intelligence;
+        } else {
+            System.out.println("Wizard's intelligence needs to be in between 1 and 50 both inclusive, insert other number.");
+        }
+    }
+
+    public void setMana(int mana) {
+        if(mana >= 10 && mana <= 50){
+            this.mana = mana;
+        } else {
+            System.out.println("Wizard's mana needs to be in between 10 and 50 both inclusive, insert other number.");
+        }
+    }
+
+    @Override
+    public void setHp(int hp){
+        if(hp >= 50 || hp <= 100){
+            super.setHp(hp);
+        } else {
+            System.out.println("Wizard's hp needs to be in between 50 and 100 both inclusive, insert other number.");
+        }
+    }
+
+
+
+    //GETTERS
+    public int getMana() {
+        return mana;
+    }
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+
+
+    //METHODS
     public int attack() {
         int damage = this.intelligence;
         if(this.intelligence>5){
@@ -21,21 +70,5 @@ public class Wizard extends Character{
             this.mana = this.mana - 1;
             return 2;
         }
-    }
-
-    public int getMana() {
-        return mana;
-    }
-
-    public void setMana(int mana) {
-        this.mana = mana;
-    }
-
-    public int getIntelligence() {
-        return intelligence;
-    }
-
-    public void setIntelligence(int intelligence) {
-        this.intelligence = intelligence;
     }
 }
