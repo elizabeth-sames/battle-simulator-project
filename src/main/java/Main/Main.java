2f7f399ada3695725d33b064cb364fedf66d74b1
AnaMoralesPuga <101170080+AnaMoralesPuga@users.noreply.github.com>
2022-04-28 17:58:38 +0200

    add output.txt (#14)

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,22 +1,36 @@
 package Main;
 
+
 import classes.Battle;
 import classes.Party;
 import classes.Warrior;
 import classes.Wizard;
 
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 import java.io.FileNotFoundException;
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.random.RandomGenerator;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+
+    public static void main(String[] args) throws IOException {
+
         Scanner scanner= new Scanner(System.in);
+
+
+        FileWriter writer = new FileWriter("Output.txt",true);
+
 
         //get player info
         System.out.println("Welcome to the Battle!");
+        writer.write("Welcome to the Battle!\n\n\n");
         System.out.println("Please enter your name: ");
         String playerName = scanner.nextLine();
         System.out.println("Welcome to the battle, " + playerName + "! Time to build your battle party." +
@@ -25,6 +39,7 @@ public class Main {
                 "\nEnter C to create a customized party." +
                 "\nEnter U to upload a party from a CSV file."
         );
+
         String[] partyOptions = {"r", "c", "u"};
         String partyChoice = scanner.nextLine().toLowerCase();
         while(!Arrays.asList(partyOptions).contains(partyChoice)) {
@@ -52,19 +67,20 @@ public class Main {
         }
         //scanner.close();
         System.out.println("Your party is ready for battle!");
-
         //create opponent team
         Party opponentsParty = new Party();
         opponentsParty.makeRandomParty();
 
         //start battle
         Battle battle = new Battle(playersParty, opponentsParty);
-        //Scanner scanner3 = new Scanner(System.in);
+
         while (playersParty.getSize() > 0 && opponentsParty.getSize() > 0) {
             //player chooses fighter
             System.out.println("Choose your fighter:");
+            writer.write("Your battle party had those members:  \n");
             for (int i=0; i<playersParty.getSize(); i++) {
                 System.out.println("Enter " + (i+1) + " for " + playersParty.getMembers().get(i).toString());
+                writer.write( playersParty.getMembers().get(i).toString() +"\n");
             }
 
             int indexPlayer = scanner.nextInt() - 1;
@@ -73,13 +89,18 @@ public class Main {
                 indexPlayer = scanner.nextInt() - 1;
             }
             System.out.println("You have chosen: " + playersParty.getMembers().get(indexPlayer).toString());
+            writer.write("You have chosen: " + playersParty.getMembers().get(indexPlayer).toString() +"\n");
             //choose opponent's fighter
             int indexOpponent = (int) Math.floor(Math.random() * opponentsParty.getSize());
             System.out.println("Your opponent has chosen: "
                     + opponentsParty.getMembers().get(indexOpponent).toString());
+            writer.write("Your opponent has chosen: "
+                    + opponentsParty.getMembers().get(indexOpponent).toString() +"\n");
+            writer.write("===========================================================================\n");
             //fight
             battle.twoPlayersFight(indexPlayer, indexOpponent);
             System.out.println("Graveyard: " + battle.getGraveyardList().toString());
+            writer.write("Graveyard: " + battle.getGraveyardList().toString() +"\n");
 
         }
         //add winner statement here
@@ -99,12 +120,11 @@ public class Main {
                     "                                      ");
         }
 
-
-
+        writer.close();
         scanner.close();
+
 
     }
 
 }
-
 
